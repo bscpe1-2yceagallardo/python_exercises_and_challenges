d34c395b6f2d335470d8d536f2ffa30f0cf687d6
Ycea Gabrielle C. Gallardo <yceagabriellecgallardo@iskolarngbayan.pup.edu.ph>
2026-03-08 20:52:09 +0800

    First commit

---
+++ sum_of_two_numbers.py
@@ -0,0 +1,9 @@
+# Create a program that ask user to input 2 numbers. Print the sum of the two numbers.
+
+# Ask numbers
+num1 = float(input("Please input first number:  "))
+num2 = float(input("Please input second number:  "))
+
+# Sum of two numbers
+sum = num1 + num2
+print(f"The sum of the two numbers is {sum}.")
