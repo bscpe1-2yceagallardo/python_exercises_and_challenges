7779199f89f7b85e8c92559684d1b2bca0508af1
Ycea Gabrielle C. Gallardo <yceagabriellecgallardo@iskolarngbayan.pup.edu.ph>
2026-03-08 21:56:44 +0800

    First commit

---
+++ quotient_of_two_numbers_with_decimal.py
@@ -0,0 +1,7 @@
+# Ask two numbers
+num1 = float(input("Please input first name:  "))
+num2 = float(input("Please input second name:  "))
+
+# Quotient of two numbers with decimal
+quotient = num1 / num2
+print(f"The quotient of {num1} and {num2} is {quotient}")
