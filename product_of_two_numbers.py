24fa116978388167d7deb50a6655fcbc5abd1162
Ycea Gabrielle C. Gallardo <yceagabriellecgallardo@iskolarngbayan.pup.edu.ph>
2026-03-08 21:40:53 +0800

    First commit

---
+++ product_of_two_numbers.py
@@ -0,0 +1,9 @@
+# Create a program that ask user to input 2 numbers. Print the product of the two numbers.
+
+# Ask two numbers
+num1 = float(input("Please input first number:  "))
+num2 = float(input("Please input second number:  "))
+
+# Product of two numbers
+product = num1 * num2
+print(f"The product of {num1} and {num2} is {product}.")
