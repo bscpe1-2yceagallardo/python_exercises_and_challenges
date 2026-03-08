29bd0e63d797a7e446361d3d200e97d39281e146
Ycea Gabrielle C. Gallardo <yceagabriellecgallardo@iskolarngbayan.pup.edu.ph>
2026-03-08 22:29:47 +0800

    First commit

---
+++ first_number_raised_to_second_number.py
@@ -0,0 +1,7 @@
+# Ask two numbers
+base_num1 = int(input("Please input first number as base:  "))
+exponent_num2 = int(input("Please input second number as exponent:  "))
+
+# First number raising to second number
+raised = base_num1 ** exponent_num2
+print(f"The result of {base_num1} being raised by {exponent_num2} is {raised}")
