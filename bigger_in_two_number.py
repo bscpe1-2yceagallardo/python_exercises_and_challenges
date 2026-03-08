1eecd359e3d4186f32cb1d2d26a73cdacf311b15
Ycea Gabrielle C. Gallardo <yceagabriellecgallardo@iskolarngbayan.pup.edu.ph>
2026-03-08 19:38:28 +0800

    First commit

---
+++ bigger_in_two_number.py
@@ -0,0 +1,11 @@
+# Ask 2 numbers
+num1 = int(input("Please input first number:  "))
+num2 = int(input("Please input second number: "))
+
+# Bigger number
+if num1 > num2:
+    print(f"The bigger number is: {num1}")
+elif num2 > num1:
+    print(f"The bigger number is: {num2}")
+else:
+    print(f"The numbers are equal.")
