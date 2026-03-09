757577b7de1ab8d049051a27657f08ca7deea75d
Ycea Gabrielle C. Gallardo <yceagabriellecgallardo@iskolarngbayan.pup.edu.ph>
2026-03-10 01:32:18 +0800

    First commit

---
+++ two_equal_numbers.py
@@ -0,0 +1,13 @@
+# Ask numbers
+num1 = int(input("Please input first number:  "))
+num2 = int(input("Please input second number:  "))
+
+# Print "Equal" if same
+if num1 == num2:
+    print(f"{num1} is Equal to {num2}")
+else:
+    print(f"{num1} is NOT Equal to {num2}")
+
+
+
+
