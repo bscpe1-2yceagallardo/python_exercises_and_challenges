3f6f3afa24f8b065cd1427d9b0045edbcebadbf4
Ycea Gabrielle C. Gallardo <yceagabriellecgallardo@iskolarngbayan.pup.edu.ph>
2026-03-10 23:29:05 +0800

    First commit

---
+++ remainder_of_two_numbers.py
@@ -0,0 +1,7 @@
+# Ask two numbers
+num1 = float(input("Please input first number to find the remainder:  "))
+num2 = float(input("Please input second number to find the remainder:  "))
+
+# Remainder of two numbers
+remainder = num1 % num2
+print(f"The remainder of {num1} and {num2} is {remainder}.")
