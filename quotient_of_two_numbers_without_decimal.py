643a392a200827bdfd1d1d40c46eeb619c018f88
Ycea Gabrielle C. Gallardo <yceagabriellecgallardo@iskolarngbayan.pup.edu.ph>
2026-03-10 23:23:34 +0800

    First commit

---
+++ quotient_of_two_numbers_without_decimal.py
@@ -0,0 +1,7 @@
+# Ask two numbers
+num1 = int(input("Please input the first number without decimal to divide:  "))
+num2 = int(input("Please input the second number without decimal to divide:  "))
+
+# Quotient without decimal
+divide = num1 // num2
+print(f"The quotient of {num1} and {num2} is {divide}.")
