330682edce1df645450e8bc7ffb977b7ab7006e5
Ycea Gabrielle C. Gallardo <yceagabriellecgallardo@iskolarngbayan.pup.edu.ph>
2026-03-10 23:01:05 +0800

    First commit

---
+++ difference_of_two_numbers.py
@@ -0,0 +1,7 @@
+# Ask two numbers
+num1 = float(input("Please input the first number to subtract:  "))
+num2 = float(input("Please input the second number to subtract:  "))
+
+# Difference of two numbers
+subtract = num1 - num2
+print(f"The subtraction of {num1} and {num2} is {subtract}")
