15f5d733126f37beea3bd332f3fa5e6aba205e21
Ycea Gabrielle C. Gallardo <yceagabriellecgallardo@iskolarngbayan.pup.edu.ph>
2026-03-10 22:55:00 +0800

    First commit

---
+++ not_two_equal_numbers.py
@@ -0,0 +1,9 @@
+# Ask two numbers
+num1 = float(input("Please input first number:  "))
+num2 = float(input("Please input second number:  "))
+
+# Not equal numbers
+if num1 != num2:
+    print(f"{num1} is NOT Equal to {num2}")
+else:
+    print(f"{num1} is Equal to {num2}")
