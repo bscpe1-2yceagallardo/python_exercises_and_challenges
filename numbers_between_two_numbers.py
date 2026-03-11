2f8fb3a63003d4480e2c85c03a3ee590d5d0cd51
Ycea Gabrielle C. Gallardo <yceagabriellecgallardo@iskolarngbayan.pup.edu.ph>
2026-03-12 01:42:30 +0800

    Last commit: printing numbers between two numbers

--- numbers_between_two_numbers.py
+++ numbers_between_two_numbers.py
@@ -4,3 +4,6 @@ last = int(input("Please input last number:  "))
 
 # Numbers between
 numbers_between = list(range(first + 1, last))
+
+# Printing numbers between two numbers
+print(f"The number/s between {first} and {last} is/are {numbers_between}.")
