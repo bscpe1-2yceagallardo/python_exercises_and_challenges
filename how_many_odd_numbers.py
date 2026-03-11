f6d104be7a4768089b28038dbef3976e663ef764
Ycea Gabrielle C. Gallardo <yceagabriellecgallardo@iskolarngbayan.pup.edu.ph>
2026-03-11 20:56:42 +0800

    First commit

--- how_many_odd_numbers.py
+++ how_many_odd_numbers.py
@@ -1,5 +1,9 @@
+odd_numbers = 0
 # How many Odd numbers
 for i in range(10):
     # Ask ten numbers
     input_numbers = int(input("Please input ten numbers:  "))
-    if input_numbers
+    # If odd numbers
+    if input_numbers % 2 != 0:
+        odd_numbers += 1
+print(f"The count of Odd numbers is {odd_numbers}.")
