2ff6fadbc0af2be5bd2f1d312591a3557c034984
Ycea Gabrielle C. Gallardo <yceagabriellecgallardo@iskolarngbayan.pup.edu.ph>
2026-03-12 00:26:48 +0800

    Second commit: added condition for first number minus remainings

--- first_number_minus__nine_remaining_numbers.py
+++ first_number_minus__nine_remaining_numbers.py
@@ -1,2 +1,9 @@
-# Ask ten numbers
-numbers = int(input("Please input ten numbers. First number to be subtracted by nine numbers:  "))
+result = 0
+# Loop of first number minus remaining numbers
+for i in range(10):
+    # Ask ten numbers
+    numbers = int(input("Please input ten numbers. First number to be subtracted by nine numbers:  "))
+    if i == 0:
+        result = numbers
+    else:
+         result -= numbers
