cd1a6ca4b5aa050b6dcd91d28ec42031193895c3
Ycea Gabrielle C. Gallardo <yceagabriellecgallardo@iskolarngbayan.pup.edu.ph>
2026-03-11 22:12:57 +0800

    First commit

---
+++ first_number_minus__nine_remaining_numbers.py
@@ -0,0 +1,10 @@
+result = 0
+# First number minus remaining
+for i in range(10):
+    # Ask ten numbers
+    numbers = int(input("Please input ten numbers. First number to be subtracted by nine numbers:  "))
+    if i == 0:
+        result = numbers
+    else:
+        result -= numbers
+print(f"The answer for first number minus remaining number is {result}.")
