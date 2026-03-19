302cc4cf8cc7187d819fe556ebb3494c060d1bf3
Ycea Gabrielle C. Gallardo <yceagabriellecgallardo@iskolarngbayan.pup.edu.ph>
2026-03-19 21:37:54 +0800

    Second commit: added logic for no duplicate numbers

--- all_numbers_without_duplicate.py
+++ all_numbers_without_duplicate.py
@@ -1,4 +1,6 @@
 # Ask 10 numbers
-numbers = int(input("Please input ten numbers: "))
+numbers = [int(input("Please input ten numbers: ")) for _ in range(10)]
 
-
+# Loop determining numbers with no duplicate
+for i in numbers:
+    if numbers.count(i) == 1:
