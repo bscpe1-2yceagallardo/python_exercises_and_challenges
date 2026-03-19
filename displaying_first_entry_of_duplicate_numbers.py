915775fc751e92fd07d50a18b2a82d52b1b16104
Ycea Gabrielle C. Gallardo <yceagabriellecgallardo@iskolarngbayan.pup.edu.ph>
2026-03-19 21:55:35 +0800

    Second commit: added logic for displaying first entry of duplicate numbers

--- displaying_first_entry_of_duplicate_numbers.py
+++ displaying_first_entry_of_duplicate_numbers.py
@@ -1,2 +1,10 @@
-# Ask 10 numbers
-    num = int(input("Please input ten numbers: "))
+numbers = []
+unique_numbers = []
+
+# Loop for displaying first entry of duplicate numbers
+for i in range(10):
+    # Ask 10 numbers
+    num = int(input(f"Please input number {i+1}: "))
+    numbers.append(num)
+    if num not in unique_numbers:
+        unique_numbers.append(num)
