a1d13016af95be0d60fd65104de8102734916c39
Ycea Gabrielle C. Gallardo <yceagabriellecgallardo@iskolarngbayan.pup.edu.ph>
2026-03-20 12:05:00 +0800

    Additional commit: added proper variables

--- all_numbers_without_duplicate.py
+++ all_numbers_without_duplicate.py
@@ -1,5 +1,5 @@
 # Ask 10 numbers
-numbers = [int(input("Please input ten numbers: ")) for _ in range(10)]
+numbers = [int(input("Please input ten numbers: ")) for i in range(10)]
 
 # Loop determining numbers with no duplicate
 for i in numbers:
