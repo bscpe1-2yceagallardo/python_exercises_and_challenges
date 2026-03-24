907c53d6abb49a11c696a9b12227268bb4527d73
Ycea Gabrielle C. Gallardo <yceagabriellecgallardo@iskolarngbayan.pup.edu.ph>
2026-03-24 22:04:55 +0800

    Last commit: Printing and condition for lowest number

--- lowest_number_until_invalid.py
+++ lowest_number_until_invalid.py
@@ -2,6 +2,12 @@ nums = []
 # Logic of until valid
 while True:
     # Ask numbers
-    input_numbers = int(input("Enter a number: "))
+    input_numbers = (input("Enter a number: "))
     if not input_numbers.isdigit(): break
-    nums.append(input_numbers)
+    nums.append(int(input_numbers))
+
+# Printing and condition for lowest number
+if nums:
+    print("Lowest number:", min(nums))
+else:
+    print("No valid numbers entered.")
