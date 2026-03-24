31a775c1432179e61c04efbc965cb740db1006a9
Ycea Gabrielle C. Gallardo <yceagabriellecgallardo@iskolarngbayan.pup.edu.ph>
2026-03-24 22:08:33 +0800

    Second commit: added logic of until valid

--- highest_number_until_invalid.py
+++ highest_number_until_invalid.py
@@ -1,3 +1,8 @@
-# Ask for numbers
-input_numbers = input("Enter a number: ")
+nums = []
+# Loop for until valid
+while True:
+    # Ask for numbers
+    input_numbers = input("Enter a number: ")
+    if not input_numbers.isdigit(): break
+    nums.append(int(input_numbers))
 
