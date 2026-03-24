25ad0a2aae2ab523448856b631be5cc8ad0490fa
Ycea Gabrielle C. Gallardo <yceagabriellecgallardo@iskolarngbayan.pup.edu.ph>
2026-03-25 01:07:41 +0800

    Second commit: added loop checking if any letter is lowercase, it's not all uppercase

--- without_using_isupper_function.py
+++ without_using_isupper_function.py
@@ -1,2 +1,9 @@
 # Input for a text
 text = input("Enter a string: ")
+
+is_upper = True
+# Checking if any letter is lowercase, it's not all uppercase
+for characters in text:
+    if 'a' <= characters <= 'z':
+        is_upper = False
+        break
