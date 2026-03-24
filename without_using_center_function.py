1501a71e923d528229e4d3b362bb1bad0acc8b81
Ycea Gabrielle C. Gallardo <yceagabriellecgallardo@iskolarngbayan.pup.edu.ph>
2026-03-25 01:41:53 +0800

    Second commit: added same function without center() function

--- without_using_center_function.py
+++ without_using_center_function.py
@@ -1,3 +1,10 @@
 # Input for a text and total width
 text = input("Enter a string: ")
 width = int(input("Enter the total width: "))
+
+# Same function without center() function
+spaces = width - len(text)
+if spaces > 0:
+    left = spaces // 2
+    right = spaces - left
+    text = " " * left + text + " " * right
