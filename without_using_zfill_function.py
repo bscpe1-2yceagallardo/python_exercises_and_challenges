85c492d46b0a3460dc64863b8ffdacd72c92f11f
Ycea Gabrielle C. Gallardo <yceagabriellecgallardo@iskolarngbayan.pup.edu.ph>
2026-03-25 02:04:26 +0800

    Second commit: added same function but without zfill() function

--- without_using_zfill_function.py
+++ without_using_zfill_function.py
@@ -1,3 +1,8 @@
 # Input for a text and total width
 text = input("Enter a number string: ")
 width = int(input("Enter the total width: "))
+
+# Same function but without zfill() function
+spaces = width - len(text)
+if spaces > 0:
+    text = "0" * spaces + text
