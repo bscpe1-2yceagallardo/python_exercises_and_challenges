972fee52383ef97782aa013aa7af1d38b0b20f02
Ycea Gabrielle C. Gallardo <yceagabriellecgallardo@iskolarngbayan.pup.edu.ph>
2026-03-25 01:51:03 +0800

    Second commit: added same function without capitalize() function

--- without_using_capitalize_function.py
+++ without_using_capitalize_function.py
@@ -1,2 +1,17 @@
 # Input for a text
 text = input("Enter a string: ")
+
+# Same function without capitalize() function
+if len(text) > 0:
+    result = ""
+
+    for i in range(len(text)):
+        characters = text[i]
+        # First character → uppercase
+        if i == 0 and 'a' <= characters <= 'z':
+            result += chr(ord(characters) - 32)
+        # Remaining characters → lowercase
+        elif i != 0 and 'A' <= characters <= 'Z':
+            result += chr(ord(characters) + 32)
+        else:
+            result += characters
