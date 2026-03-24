6859a69866a0859cab5e41c71a0d510c820753f0
Ycea Gabrielle C. Gallardo <yceagabriellecgallardo@iskolarngbayan.pup.edu.ph>
2026-03-24 23:11:15 +0800

    Last commit: added condition of number with most duplicates

--- most_duplicate_number_until_invalid.py
+++ most_duplicate_number_until_invalid.py
@@ -5,3 +5,10 @@ while True:
     input_numbers = input("Enter a number: ")
     if not input_numbers.isdigit(): break
     nums.append(int(input_numbers))
+
+# Condition of number with most duplicates
+if nums:
+    most = max(nums, key=nums.count)
+    print("Number with most duplicates:", most)
+else:
+    print("No valid numbers entered.")
