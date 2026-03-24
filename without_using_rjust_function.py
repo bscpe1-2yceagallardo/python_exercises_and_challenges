afc0e602dd836df18418ab82e17e135564db7c21
Ycea Gabrielle C. Gallardo <yceagabriellecgallardo@iskolarngbayan.pup.edu.ph>
2026-03-25 01:31:31 +0800

    Second commit: added condition of adding spaces at the beginning to reach desired width

--- without_using_rjust_function.py
+++ without_using_rjust_function.py
@@ -1,3 +1,8 @@
 # Input for a text and total width
 text = input("Enter a string: ")
 width = int(input("Enter the total width: "))
+
+# Condition of adding spaces at the beginning to reach desired width
+spaces = width - len(text)
+if spaces > 0:
+    text = " " * spaces + text
