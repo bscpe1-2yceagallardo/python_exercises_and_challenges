ac7f2d2c43d2d9c74e94d064d9f33fbaf5ec1b78
Ycea Gabrielle C. Gallardo <yceagabriellecgallardo@iskolarngbayan.pup.edu.ph>
2026-03-25 01:15:55 +0800

    Last commit: printing and checking if the last part of the string matches the suffix

--- without_using_endswith_function.py
+++ without_using_endswith_function.py
@@ -1,3 +1,9 @@
 # Input for a text and suffix to check
 text = input("Enter a string: ")
 suffix = input("Enter the suffix to check: ")
+
+# Printing and checking if the last part of the string matches the suffix
+if len(suffix) > len(text):
+    print(False)
+else:
+    print(text[-len(suffix):] == suffix)
