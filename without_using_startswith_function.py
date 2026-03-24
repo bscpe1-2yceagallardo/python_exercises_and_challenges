c3d746e2070ebe8173e83526ccddcf369b9d184e
Ycea Gabrielle C. Gallardo <yceagabriellecgallardo@iskolarngbayan.pup.edu.ph>
2026-03-25 01:18:42 +0800

    Last commit: printing and checking if the first part of the string matches the prefix

--- without_using_startswith_function.py
+++ without_using_startswith_function.py
@@ -1,3 +1,9 @@
 # Input for a text and prefix to check
 text = input("Enter a string: ")
 prefix = input("Enter the prefix to check: ")
+
+# Printing and checking if the first part of the string matches the prefix
+if len(prefix) > len(text):
+    print(False)
+else:
+    print(text[:len(prefix)] == prefix)
