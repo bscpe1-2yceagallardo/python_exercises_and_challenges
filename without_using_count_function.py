281b4d079a24ec125b79dd76f4813089aefbb0f6
Ycea Gabrielle C. Gallardo <yceagabriellecgallardo@iskolarngbayan.pup.edu.ph>
2026-03-25 02:08:28 +0800

    Second commit: added same function but without count() function

--- without_using_count_function.py
+++ without_using_count_function.py
@@ -1,3 +1,9 @@
 # Input for a text and character to count
 text = input("Enter a string: ")
 char = input("Enter the character to count: ")
+
+# Same function but without count() function
+count = 0
+for character in text:
+    if character == char:
+        count += 1
