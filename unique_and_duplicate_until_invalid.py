569bf0890609a0129091584765a2243288a2425c
Ycea Gabrielle C. Gallardo <yceagabriellecgallardo@iskolarngbayan.pup.edu.ph>
2026-03-24 23:04:29 +0800

    Last commit: added condition for Unique and Duplicate

--- unique_and_duplicate_until_invalid.py
+++ unique_and_duplicate_until_invalid.py
@@ -4,3 +4,8 @@ while True:
     # Ask for numbers
     input_numbers = input("Enter a number: ")
     if not input_numbers.isdigit(): break
+
+    # Condition for "Unique" and "Duplicate"
+    if_num = int(input_numbers)
+    print("Duplicate" if if_num in nums else "Unique")
+    nums.add(if_num)
