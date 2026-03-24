c14a104196ea0106c040965e3fb90aeb2357c4f3
Ycea Gabrielle C. Gallardo <yceagabriellecgallardo@iskolarngbayan.pup.edu.ph>
2026-03-24 22:18:20 +0800

    Last commit: Printing and sorting lowest to highest

--- lowest_to_highest_until_invalid.py
+++ lowest_to_highest_until_invalid.py
@@ -6,4 +6,7 @@ while True:
     if not input_numbers.isdigit(): break
     nums.append(int(input_numbers))
 
+# Printing and sorting lowest to highest
+nums.sort()
+print("Numbers from lowest to highest:", nums)
 
