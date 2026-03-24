20b31d4da58f1369d6fe2bf99f87f246a712626a
Ycea Gabrielle C. Gallardo <yceagabriellecgallardo@iskolarngbayan.pup.edu.ph>
2026-03-24 22:48:38 +0800

    First commit: user input prompts for ten numbers

--- displaying_first_entry_of_duplicate_numbers.py
+++ displaying_first_entry_of_duplicate_numbers.py
@@ -1,18 +1,4 @@
+# Ask 10 numbers
 numbers = []
-unique_numbers = []
-
-# Loop for displaying first entry of duplicate numbers
 for i in range(10):
-    # Ask 10 numbers
-    num = int(input(f"Please input number {i+1}: "))
-    numbers.append(num)
-    if num not in unique_numbers:
-        unique_numbers.append(num)
-
-# Print all numbers entered
-print("\nAll numbers entered:")
-print(numbers)
-
-# Print numbers without duplicates (only first occurrence)
-print("\nNumbers without duplicates:")
-print(unique_numbers)
+    numbers.append(int(input("Enter a number: ")))
