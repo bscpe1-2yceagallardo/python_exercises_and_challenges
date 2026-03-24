421f8f56dda49de4ccacae05c9e3ad191f6ac4da
Ycea Gabrielle C. Gallardo <yceagabriellecgallardo@iskolarngbayan.pup.edu.ph>
2026-03-25 00:28:35 +0800

    Last commit: printing and checking if the start of 'text' matches 'prefix'

--- without_using_removeprefix_function.py
+++ without_using_removeprefix_function.py
@@ -4,3 +4,9 @@ prefix = input("Enter the prefix to remove: ")
 
 # Get the length of the prefix
 length_prefix = len(prefix)
+
+# Printing and checking if the start of 'text' matches 'prefix'
+if text[:length_prefix] == prefix:
+    print("After removeprefix():", text[length_prefix:])
+else:
+    print("After removeprefix():", text) 
