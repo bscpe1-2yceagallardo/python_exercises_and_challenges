7d2451c7485fcf5f34aa62d41d1da34020b00a09
Ycea Gabrielle C. Gallardo <yceagabriellecgallardo@iskolarngbayan.pup.edu.ph>
2026-03-25 01:44:25 +0800

    Second commit: added same function without swapcase() function

--- without_using_swapcase_function.py
+++ without_using_swapcase_function.py
@@ -1,2 +1,14 @@
 # Input for a text
 text = input("Enter a string: ")
+
+# Same function without swapcase() function
+result = ""
+for characters in text:
+    # Convert uppercase to lowercase
+    if 'A' <= characters <= 'Z':
+        result += chr(ord(characters) + 32)
+    # Convert lowercase to uppercase
+    elif 'a' <= characters <= 'z':
+        result += chr(ord(characters) - 32)
+    else:
+        result += characters  # Non-alphabet characters remain unchanged
