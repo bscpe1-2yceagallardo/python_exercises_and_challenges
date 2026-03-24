938ce1673900c32ce88c90ba5a32746190037122
Ycea Gabrielle C. Gallardo <yceagabriellecgallardo@iskolarngbayan.pup.edu.ph>
2026-03-25 02:21:03 +0800

    Second commit: added same function but without rindex() function

--- without_using_rindex_function.py
+++ without_using_rindex_function.py
@@ -1,3 +1,11 @@
 # Input for a text and substring to find
 text = input("Enter a string: ")
 substr = input("Enter the substring to find: ")
+
+# Same function but without rindex() function
+found = False
+for i in range(len(text) - len(substr), -1, -1):  # Loop backwards
+    if text[i:i+len(substr)] == substr:
+        print("Last occurrence index:", i)
+        found = True
+        break
