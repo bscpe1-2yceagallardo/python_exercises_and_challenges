baaf7023795add069201cc7d796c3b061f357ef9
Ycea Gabrielle C. Gallardo <yceagabriellecgallardo@iskolarngbayan.pup.edu.ph>
2026-03-25 00:33:02 +0800

    Last commit: printing and checking if the end of 'text' matches 'suffix'

--- without_using_removesuffix_function.py
+++ without_using_removesuffix_function.py
@@ -4,3 +4,9 @@ suffix = input("Enter the suffix to remove: ")
 
 # Get the length of the suffix
 n = len(suffix)
+
+# Printing and checking if the end of 'text' matches 'suffix'
+if text[-n:] == suffix:
+    print("After removesuffix():", text[:-n])
+else:
+    print("After removesuffix():", text)
