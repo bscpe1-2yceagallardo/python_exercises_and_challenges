f6160eaee08aaf8433c5e7caed4864dd52137af5
Ycea Gabrielle C. Gallardo <yceagabriellecgallardo@iskolarngbayan.pup.edu.ph>
2026-03-25 02:19:06 +0800

    Second commit: added same function but without index() function

--- without_using_index_function.py
+++ without_using_index_function.py
@@ -1,3 +1,11 @@
 # Input for a text and substring to find
 text = input("Enter a string: ")
 substr = input("Enter the substring to find: ")
+
+# Same function but without index() function
+found = False
+for i in range(len(text) - len(substr) + 1):
+    if text[i:i+len(substr)] == substr:
+        print("First occurrence index:", i)
+        found = True
+        break
