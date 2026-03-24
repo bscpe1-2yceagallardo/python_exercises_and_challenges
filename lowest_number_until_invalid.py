6813ae5f960ce59ff81d53a2f4a1ba6bb8838942
Ycea Gabrielle C. Gallardo <yceagabriellecgallardo@iskolarngbayan.pup.edu.ph>
2026-03-24 22:02:05 +0800

    Second commit: added logic of until valid

--- lowest_number_until_invalid.py
+++ lowest_number_until_invalid.py
@@ -1,3 +1,7 @@
-# Ask numbers
-input_numbers = int(input("Enter a number: "))
-
+nums = []
+# Logic of until valid
+while True:
+    # Ask numbers
+    input_numbers = int(input("Enter a number: "))
+    if not input_numbers.isdigit(): break
+    nums.append(input_numbers)
