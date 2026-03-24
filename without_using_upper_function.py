1c9f6ce6ced050c118240381e5161f1158b0f3d7
Ycea Gabrielle C. Gallardo <yceagabriellecgallardo@iskolarngbayan.pup.edu.ph>
2026-03-25 00:57:16 +0800

    Second commit: added loop for converting lowercase letters to uppercase

--- without_using_upper_function.py
+++ without_using_upper_function.py
@@ -1,2 +1,10 @@
 # Input for a text
 text = input("Enter a string: ")
+result = ""
+
+# Loop for converting lowercase letters to uppercase
+for i in text:
+    if 'a' <= i <= 'z':
+        result +=chr(ord(i)-32)
+    else:
+        result += i
